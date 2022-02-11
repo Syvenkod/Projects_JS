172d6c3bf361154e7b935a807b5b9e17a7f39101
Dmytro Syvenko <syvenkod@gmail.com>
2022-02-11 20:22:10 +0200

    1

--- first/main.js
+++ first/main.js
@@ -2,27 +2,38 @@
 let table = document.querySelector("#table");
 let btnTempC = document.querySelector("#btn-tempC");
 let btnTempF = document.querySelector("#btn-tempF");
+let gradus = "&units=metric";
 
-btnTempC.addEventListener("click",function(){
-table.innerHTML = "";
-let city1 = new TodayCityWeather("http://api.openweathermap.org/data/2.5/weather?id=703448&appid=bf35cac91880cb98375230fb443a116f&units=metric");
-let city2 = new TodayCityWeather("http://api.openweathermap.org/data/2.5/weather?id=2643743&appid=bf35cac91880cb98375230fb443a116f&units=metric");
-let city3 = new TodayCityWeather("http://api.openweathermap.org/data/2.5/weather?id=5128638&appid=bf35cac91880cb98375230fb443a116f&units=metric");
+let city1 = new TodayCityWeather(`http://api.openweathermap.org/data/2.5/weather?id=703448&appid=bf35cac91880cb98375230fb443a116f${gradus}`);
+// let city2 = new TodayCityWeather(`http://api.openweathermap.org/data/2.5/weather?id=2643743&appid=bf35cac91880cb98375230fb443a116f${gradus}`);
+// let city3 = new TodayCityWeather(`http://api.openweathermap.org/data/2.5/weather?id=5128638&appid=bf35cac91880cb98375230fb443a116f${gradus}`);
+
 city1.todayInfo;
-city2.todayInfo;
-city3.todayInfo;
-});
+// city2.todayInfo();
+// city3.todayInfo();
 
-btnTempF.addEventListener("click",function(){
-table.innerHTML = "";
-let city1 = new TodayCityWeather("http://api.openweathermap.org/data/2.5/weather?id=703448&appid=bf35cac91880cb98375230fb443a116f&units=imperial");
-let city2 = new TodayCityWeather("http://api.openweathermap.org/data/2.5/weather?id=2643743&appid=bf35cac91880cb98375230fb443a116f&units=imperial");
-let city3 = new TodayCityWeather("http://api.openweathermap.org/data/2.5/weather?id=5128638&appid=bf35cac91880cb98375230fb443a116f&units=imperial");
-city1.todayInfo;
-city2.todayInfo;
-city3.todayInfo;
- });
+// btnTempC.addEventListener("click", function(){
 
-window.addEventListener("load", function () {
+// gradus = "&units=metric";
 
-});
+// })
+
+// btnTempF.addEventListener("click",function(){
+// table.innerHTML = "";
+// let city1 = new TodayCityWeather(`http://api.openweathermap.org/data/2.5/weather?id=703448&appid=bf35cac91880cb98375230fb443a116f&units=imperial`);
+// let city2 = new TodayCityWeather(`http://api.openweathermap.org/data/2.5/weather?id=2643743&appid=bf35cac91880cb98375230fb443a116f&units=imperial`);
+// let city3 = new TodayCityWeather(`http://api.openweathermap.org/data/2.5/weather?id=5128638&appid=bf35cac91880cb98375230fb443a116f&units=imperial`);
+// city1.todayInfo();
+// city2.todayInfo();
+// city3.todayInfo();
+//  });
+
+ let cities = ["703448", "2643743", "5128638"];
+ let cityArray = [];
+ cities.forEach(cityId => {
+     cityArray.push(new TodayCityWeather(`http://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=bf35cac91880cb98375230fb443a116f${gradus}`))
+ })
+ console.log(cityArray);
+
+
+
