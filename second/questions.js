3cf38090890ca49cd428c15b8dc47c3eaa1851c9
Syvenkod <95404521+Syvenkod@users.noreply.github.com>
2022-02-15 13:23:07 +0200

    radio buttons

--- second/questions.js
+++ second/questions.js
@@ -8,7 +8,8 @@ for(var i = 10; i < 35; i += 5) {
 new Answer ("10", 0),
 new Answer ("5", 1),
 new Answer ("15", 0),
-new Answer ("25", 0)]),
+new Answer ("25", 0)],
+"1"),
 
 new Question (`Что будет на экране?
     alert(str);
@@ -17,28 +18,30 @@ new Question (`Что будет на экране?
 new Answer ("Hello", 0),
 new Answer ("alert", 0),
 new Answer ("будет ошибка", 0),
-new Answer ("undefined", 1)]),
+new Answer ("undefined", 1)],
+"2"),
 
 new Question (`Как объявить функцию в JavaScript?`,
 [
 new Answer ("function:MyFunction()", 0),
 new Answer ("function MyFunction()", 1),
 new Answer ("function = MyFunction()", 0),
-new Answer ("function = New MyFunction()", 0)]),
+new Answer ("function = New MyFunction()", 0)],
+"3"),
 
 new Question (`Какое из этих ключевых слов ООП не используется в JavaScript?`,
 [
 new Answer ("this", 0),
 new Answer ("super", 0),
 new Answer ("все есть", 1),
-new Answer ("instanceOf", 0)]),
+new Answer ("instanceOf", 0)], "4"),
 
 new Question (`JSON - это...`,
 [
 new Answer ("JavaScript Object Notation", 1),
 new Answer ("название следующей версии JavaScript", 0),
 new Answer ("JavaScript Over Network", 0),
-new Answer ("имя создателя JavaScript", 0)]),
+new Answer ("имя создателя JavaScript", 0)], "5"),
 
 new Question (`Какое сообщение покажет alert?
                  let i = 5;
@@ -47,14 +50,14 @@ new Question (`Какое сообщение покажет alert?
 new Answer ("4", 0),
 new Answer ("5", 0),
 new Answer ("6", 1),
-new Answer ("undefined", 0)]),
+new Answer ("undefined", 0)], "6"),
 
 new Question (`Какой оператор завершает выполнение текущей функции и возвращает её значение?`,
 [
 new Answer ("case", 0),
 new Answer ("break", 0),
 new Answer ("catch", 0),
-new Answer ("return", 1)]),
+new Answer ("return", 1)], "7"),
 
 new Question (`Чему равно i в конце кода?
     for(var i=0; i<10; i++) {
@@ -65,21 +68,21 @@ new Question (`Чему равно i в конце кода?
 new Answer ("undefined", 0),
 new Answer ("9", 0),
 new Answer ("10", 1),
-new Answer ("нет такой переменной после цикла", 0)]),
+new Answer ("нет такой переменной после цикла", 0)], "8"),
 
 new Question (`В чем отличие между локальной и глобальной переменной?`,
 [
 new Answer ("отличий нет", 0),
 new Answer ("локальные видны повсюду, глобальные только в функциях", 0),
 new Answer ("глобальные можно переопределять, локальные нельзя", 0),
-new Answer ("глобальные видны повсюду, локальные только в функциях", 1)]),
+new Answer ("глобальные видны повсюду, локальные только в функциях", 1)], "9"),
 
 new Question (`В чем разница между confirm и prompt?`,
 [
 new Answer ("ничем не отличаются", 0),
 new Answer ("prompt вызывает диалоговое окно с полем для ввода, confirm - окно с подтверждением", 1),
 new Answer ("confirm вызывает диалоговое окно с полем для ввода, prompt - окно с подтверждением", 0),
-new Answer ("О чем Вы?", 0)]),
+new Answer ("О чем Вы?", 0)], "10"),
 
 ]
 console.log(questions);
