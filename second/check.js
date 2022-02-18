fc1a95a83befa6d26e4dff6dc5144e81d28b2e18
Syvenkod <95404521+Syvenkod@users.noreply.github.com>
2022-02-18 12:24:51 +0200

    add check&style

--- second/check.js
+++ second/check.js
@@ -1,17 +1,29 @@
 function check (){
-    let counter = 0;
-    let answer1 = document.querySelectorAll("input[name='answer-0']");
-    checkAnswer(answer1);
+let counter = 0;
+let answer0 = Array.from(document.querySelectorAll("input[name='answer-0']"));
+let answer1 = Array.from(document.querySelectorAll("input[name='answer-1']"));
+let answer2 = Array.from(document.querySelectorAll("input[name='answer-2']"));
+let answer3 = Array.from(document.querySelectorAll("input[name='answer-3']"));
+let answer4 = Array.from(document.querySelectorAll("input[name='answer-4']"));
+let answer5 = Array.from(document.querySelectorAll("input[name='answer-5']"));
+let answer6 = Array.from(document.querySelectorAll("input[name='answer-6']"));
+let answer7 = Array.from(document.querySelectorAll("input[name='answer-7']"));
+let answer8 = Array.from(document.querySelectorAll("input[name='answer-8']"));
+let answer9 = Array.from(document.querySelectorAll("input[name='answer-9']"));
 
-    function checkAnswer() {
-        for (let i=0; i<this.length; i++) {
-        if (this[i].checked) {
-          counter +=this[i].value;
-        }
-        console.log(counter);
-        return counter;
-}}
+let answersArray = answer0.concat(answer1, answer2, answer3, answer4, answer5, answer6, answer7, answer8, answer9);
 
-return counter;
-};
+    for (let i=0; i<answersArray.length; i++) {
+    if (answersArray[i].checked) {
+      counter +=Number(answersArray[i].value);
+    }
+}
+let result = (counter/10)*100;
+let confirmed = confirm(`Вы выполнили на ${result}% из 100%
+Хотите повторить тест?`);
+if(confirmed){window.open("test.html")}
+else {alert("Спасибо за выполнение теста")}
+}
+
+
 
