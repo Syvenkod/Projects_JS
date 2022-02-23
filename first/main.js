9a269d02ef48d0d6058696da79303bc9c23ea62d
Dmytro Syvenko <syvenkod@gmail.com>
2022-02-23 19:25:03 +0200

    23.02.2022

--- first/main.js
+++
@@ -1,8 +0,0 @@
-let table = document.querySelector("#table");
-let tableForecast = document.querySelector("#forecast");
-let cities = ["703448", "2643743", "5128638", "702550", "698740"];
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-window.addEventListener("load", showWeather());
-
-
-
