f59951f99ec87360ca416e9a43677c60117c5d47
Dmytro Syvenko <syvenkod@gmail.com>
2022-05-04 20:31:10 +0300

    refactoring

--- Test_10_questions/check.js
+++ Test_10_questions/check.js
@@ -1,18 +1,6 @@
 function check (){
 let counter = 0;
-let answer0 = Array.from(document.querySelectorAll("input[name='answer-0']"));
-let answer1 = Array.from(document.querySelectorAll("input[name='answer-1']"));
-let answer2 = Array.from(document.querySelectorAll("input[name='answer-2']"));
-let answer3 = Array.from(document.querySelectorAll("input[name='answer-3']"));
-let answer4 = Array.from(document.querySelectorAll("input[name='answer-4']"));
-let answer5 = Array.from(document.querySelectorAll("input[name='answer-5']"));
-let answer6 = Array.from(document.querySelectorAll("input[name='answer-6']"));
-let answer7 = Array.from(document.querySelectorAll("input[name='answer-7']"));
-let answer8 = Array.from(document.querySelectorAll("input[name='answer-8']"));
-let answer9 = Array.from(document.querySelectorAll("input[name='answer-9']"));
-
-let answersArray = answer0.concat(answer1, answer2, answer3, answer4, answer5, answer6, answer7, answer8, answer9);
-
+let answersArray = Array.from(document.querySelectorAll("input[name]"));
     for (let i=0; i<answersArray.length; i++) {
     if (answersArray[i].checked) {
       counter +=Number(answersArray[i].value);
